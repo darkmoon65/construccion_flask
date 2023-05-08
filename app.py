1fd9c4f660c46bc08d69b1d558720e8c9d31b164
cesar <csantillana@ulasalle.edu.pe>
2023-05-08 10:40:48 -0500

    Feature openface asistencias

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from backend.blueprints.cursos_blueprint import curso_blueprint
 from backend.blueprints.profesores_blueprint import profesor_blueprint
 from backend.blueprints.usuarios_blueprint import usuario_blueprint
 from backend.blueprints.grupos_blueprint import grupo_blueprint
+from backend.blueprints.asistencias_blueprint import asistencia_blueprint
 
 app = Flask(__name__)
 
@@ -17,6 +18,7 @@ app.register_blueprint(curso_blueprint)
 app.register_blueprint(profesor_blueprint)
 app.register_blueprint(usuario_blueprint)
 app.register_blueprint(grupo_blueprint)
+app.register_blueprint(asistencia_blueprint)
 
 cors = CORS(app)
 

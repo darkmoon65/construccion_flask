59cde28d7c62b489a20f471c50a111de7fc7896c
SharmelynVioleta <sbautistal@ulasalle.edu.pe>
2023-06-18 22:23:56 -0500

    Agregando login

--- frontend/simple_app/src/components/HomeComponent.vue
+++ frontend/simple_app/src/components/HomeComponent.vue
@@ -1,21 +1,41 @@
 <template>
     <div class='simple_component'>
         <div class='logo'>
-            <img src="../assets/logo.png" alt="">
+            <b-avatar src="https://placekitten.com/300/300" size="8rem"></b-avatar>
+        </div>
+        <div class="inputs">
+        <div class="mt-2" style="font-weight: 600; align-content:center;">Ingresa tu correo:</div>
+            <b-form-input v-model="value" type="email" debounce="500"></b-form-input>
+            <div class="mt-2" style="font-weight: 600">Ingresa tu contraseña:</div>
+            <b-form-input v-model="value" type="password" debounce="500"></b-form-input>
+        </div >
+        <div class="send">
+            <br>
+            <b-button variant="primary">Login</b-button>
         </div>
     </div>
-    </template>
-    
-    <script>
-    export default{
-    }
-    </script>
-    
+</template>
+
+<script>
+export default{
+}
+</script>
+
     <style>
         .simple_component{
             display: flex;
             align-items: center;
             justify-content: center;
+            padding-left: 43%;
+            padding-top: 6%;
+        }
+        .logo{
+            padding-left: 11%;
+        }
+        .send{
+            padding-left: 16%;
+        }
+        .inputs{
+            width: 300px;
         }
     </style>
-    
